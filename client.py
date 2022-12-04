e91f610efb0cd9fdcaf35e6fb13acdd94fec71a0
Matthew Villarica <matthewvillarica@gmail.com>
2022-12-04 16:22:29 +0800

    user registration

---
+++ client.py
@@ -0,0 +1,97 @@
+#!usr/bin/python
+
+import socket
+import json
+import sys
+
+def display_commands():
+    print('---------------------------------------------------------------------------')
+    print('Welcome to message board app! Please be informed of the following commands:')
+    print('/join <server_ip_add> <port>')
+    print('/leave')
+    print('/register <handle>')
+    print('/all <message>')
+    print('/msg <handle> <message>')
+    print('/?')
+    print('----------------------------------------------------------------------------')
+
+
+def register_user(client_handle):
+  
+    #dictionary to convert to JSON
+    register = {
+        "command" : "register", 
+        "handle" :  client_handle
+    }
+
+    return register
+
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
+
+udp_host = socket.gethostname()		# Host IP
+udp_port = 12345
+
+# client is always initially not registered
+is_registered = False	
+
+
+
+run_client = True
+# specified port to connect
+print ("UDP target IP:", udp_host)
+print ("UDP target Port:", udp_port)
+display_commands()
+
+while run_client == True:
+    
+
+    input_script = input('Enter input script: /')
+    splitted_script = input_script.split(sep=' ')
+
+    if splitted_script[0] == 'register':
+        reg_req = register_user(splitted_script[1])
+
+        try:
+            request_to_register = register_user(splitted_script[1])
+
+            #convert dictionary to JSON (for interpoability)
+            jsonRequest = json.dumps(request_to_register)
+            # send request to register to server
+            sock.sendto(bytes(jsonRequest, 'utf-8'),(udp_host,udp_port))
+            #wait for response from server
+            data, server_addr = sock.recvfrom(1024)
+
+            #convert server response to dict
+            server_response = json.loads(data)
+
+                
+            print(server_response['message'])
+        except:
+            print('Uknown error')
+
+        
+
+    elif splitted_script[0] == 'leave':
+        run_client = False
+
+    elif splitted_script[0] == '?':
+        display_commands()
+
+   
+
+
+    #wait for server response
+
+    
+
+
+
+    
+
+
+
+
+
+    
+    		
---
+++ server.py
@@ -0,0 +1,70 @@
+import socket
+import json
+import sys
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      
+
+udp_host = socket.gethostname()		        
+udp_port = 12345			                
+
+
+
+registered_users =[]
+
+sock.bind((udp_host,udp_port))
+
+#Register user if user is not yet in the list of clients and return appropriate response
+# to the client
+def register_user(request):
+	if request['handle'] in registered_users:
+		error = {
+			"command": 'error',
+			"message": 'Error: Registration failed, handle or alias already exists'
+		}
+		
+		return error
+	else:
+		registered_users.append(request['handle'])
+
+		#unsure about the implementation of this since theres no explicit "command: success"
+		# in the MP specs
+		success = {
+			"command": 'success',
+			"message": 'Registration successful. Welcome ' + request['handle']
+		}
+
+		print('Current list of registered users: ', registered_users)
+		return success
+
+
+		
+
+
+
+while True:
+	print ("Waiting for client request...")
+	
+	data,client_addr = sock.recvfrom(1024)	  
+	    
+	#convert data in JSON to python dictionary
+	dict_req = json.loads(data)
+
+	if dict_req['command'] == "register":
+
+		#process registration, results are in the form of a dict
+		res = register_user(dict_req)
+
+		print('registered dic response', res)
+		#convert dictionary to JSON (for interpoability), encode into bytes
+		json_response = json.dumps(res).encode('utf-8')
+		
+      
+		sock.sendto(json_response,client_addr)
+
+
+	
+	#print('dict_req', dict_req)
+	#print(type(dict_req))
+	#print(dict_req['command'])
+
+
