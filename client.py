a66824154b93ea4807f37c94d3360a5b55821963
eneii313 <jonaviene_capunitan@dlsu.edu.ph>
2022-12-07 03:47:49 +0800

    tried to make join

--- client.py
+++ client.py
@@ -5,75 +5,175 @@ import json
 import sys
 import threading
 
+host_ip = socket.gethostbyname(socket.gethostname())	    # Local Host IP Address
+server_port = None                                          # Used to check if the client has connected to the server
+client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
+
+
 def display_commands():
     print('---------------------------------------------------------------------------')
-    print('Welcome to message board app! Please be informed of the following commands:')
-    print('/join <server_ip_add> <port>')
-    print('/leave')
-    print('/register <handle>')
-    print('/all <message>')
-    print('/msg <handle> <message>')
-    print('/?')
+    print('LIST OF COMMANDS:')
+    print('/join <server_ip_add> <port> \t - connect to a server')
+    print('/leave \t \t \t \t - disconnect from the connected server')
+    print('/register <handle> \t \t - register a unique handle for the server')
+    print('/all <message> \t \t \t - send a message to everyone in the server')
+    print('/msg <handle> <message> \t - send a direct message to a single user')
+    print('/? \t \t \t \t - display all input commands')
     print('----------------------------------------------------------------------------')
 
+# --- Dictionary to convert to JSON ---
+def join_server():
+    join = {
+        "command": "join"
+    }
+    return join
 
 def register_user(client_handle):
-  
-    #dictionary to convert to JSON
     register = {
         "command" : "register", 
         "handle" :  client_handle
     }
-
     return register
 
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
+# IDK why but t1.start() causes a runtime error even if you remove the other client.recvfrom(1024)
+# def receiveMessages():
+#     while True:
+#         print('Listening...')
+#         try:
+#             msg, adr = client.recvfrom(1024)
 
-udp_host = socket.gethostname()		# Host IP
-udp_port = 12345
+#             dict_req = json.loads(msg)
 
+#             if dict_req['command'] == "register":
+#                 print(dict_req['message'])
+#             elif dict_req['command'] == "error":
+#                 print(dict_req['message'])
+#             else:
+#                 print("Unknown Error")
 
-run_client = True
-# specified port to connect
-print ("UDP target IP:", udp_host)
-print ("UDP target Port:", udp_port)
+#         except Exception as e:
+#             print("Error in receiving messages from server: ", e)
+    
+
+# t1 = threading.Thread(target=receiveMessages)
+# t1.start()
+
+# Starting Message
+print('Welcome to the UDP Message Board! Please be informed of the following commands:')
 display_commands()
+print('Available server:')
+print ("\t Server IP: ", host_ip)
+print ("\t Server Port: 12345")
+print()
 
-def send():
+def run():
     while True:
         input_script = input('Enter input script: /')
         splitted_script = input_script.split(sep=' ')
 
-        if splitted_script[0] == 'register':
+        if splitted_script[0] == 'join' and len(splitted_script) == 3:
+            # Try to send a command to the inputted server
+            try: 
+                join_ip = splitted_script[1]
+                join_port = int(splitted_script[2])
 
-            reg_req = register_user(splitted_script[1])
+                if join_ip != host_ip:
+                    raise Exception("Incorrect IP address.")
+                else:
+                    jsonRequest = json.dumps(join_server())
+                    client.sendto(bytes(jsonRequest, 'utf-8'), (host_ip, join_port))
+                    
+                    msg, adr = client.recvfrom(1024)
 
-            try:
-                request_to_register = register_user(splitted_script[1])
+                    server_response = json.loads(msg)
 
-                #convert dictionary to JSON (for interpoability)
-                jsonRequest = json.dumps(request_to_register)
+                    # If success, update server_port and listen to server
+                    if server_response['command'] == 'join':
+                        server_port = join_port
 
-                # send request to register to server
-                sock.sendto(bytes(jsonRequest, 'utf-8'),(udp_host,udp_port))
-
-                data, server_addr = sock.recvfrom(1024)
-
-                server_response = json.loads(data)
-
-                print(server_response['message'])
+                    print(server_response['message'])
+                
             except:
-                print('Uknown error')
+                print("> Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
 
-        elif splitted_script[0] == 'leave':
-            run_client = False
+
+        elif splitted_script[0] == 'register' and len(splitted_script) == 2:
+            
+            if server_port == None:
+                print("> Error: You need to join a server before you can register.")
+            
+            else:
+                reg_req = register_user(splitted_script[1])
+
+                try:
+                    request_to_register = register_user(splitted_script[1])
+
+                    jsonRequest = json.dumps(request_to_register)
+
+                    client.sendto(bytes(jsonRequest, 'utf-8'),(host_ip, server_port))
+
+                    msg, adr = client.recvfrom(1024)
+
+                    server_response = json.loads(msg)
+
+                    print(server_response['message'])
+
+                except:
+                    print("Error in register: ", e)
+
+        elif splitted_script[0] == 'all':
+            pass
+            
+        elif splitted_script[0] == 'leave': #WIP
+            server_port = None
+            print('> Connection closed. Thank you!')
+            break
 
         elif splitted_script[0] == '?':
             display_commands()
+        
+        else:
+            print("> Error: Command not found. Type '/?' to see the list of commands")
 
-t1 = threading.Thread(target=send)
+
+t1 = threading.Thread(target=run)
 t1.start()
+
+# def send():
+#     while True:
+#         input_script = input('Enter input script: /')
+#         splitted_script = input_script.split(sep=' ')
+
+#         if splitted_script[0] == 'register':
+
+#             reg_req = register_user(splitted_script[1])
+
+#             try:
+#                 request_to_register = register_user(splitted_script[1])
+
+#                 #convert dictionary to JSON (for interpoability)
+#                 jsonRequest = json.dumps(request_to_register)
+
+#                 # send request to register to server
+#                 client.sendto(bytes(jsonRequest, 'utf-8'),(udp_host,12345))
+
+#                 data, server_addr = client.recvfrom(1024)
+
+#                 server_response = json.loads(data)
+
+#                 print(server_response['message'])
+#             except Exception as e:
+#                 print("Error: ", e)
+
+#         elif splitted_script[0] == 'leave':
+#             run_client = False
+
+#         elif splitted_script[0] == '?':
+#             display_commands()
+
+# t1 = threading.Thread(target=send)
+# t1.start()
 
 
    
--- server.py
+++ server.py
@@ -4,28 +4,58 @@ import sys
 import queue
 import threading
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      
-udp_host = socket.gethostname()		        
+server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      
+udp_host = socket.gethostbyname(socket.gethostname())	        
 udp_port = 12345			                
 
 messages = queue.Queue()
 
-print("Server is online.")
-print("Waiting for clients...")
+# List of connected port numbers
+addresses = []
 
-# list of registered users
+# list of users
 # Registered users are stored as {username: <name>, address: <address>}
 registered_users = []
 
-sock.bind((udp_host,udp_port))
+server.bind((udp_host,udp_port))
 
-#Register user if user is not yet in the list of clients and return appropriate response to the client
+print("Server is online.")
+print("Waiting for clients...")
+
+#Add address to list of users in the server
+def join_user(address):
+	# Check if port address is already used
+	if isConnected(address):
+		error = {
+		"command": 'error',
+		"message": "> Error: Joining server failed. The user's port number is already used."
+		}
+		return error
+	# If port address is available, add to users
+	addresses.append(address)
+	success = {
+		"command": 'join',
+		"message": '> Connection to the Message Board Server is successful!'
+	}
+	print('A new user ', address, ' has connected to the server.')
+	return success
+
+
+#Register user if user has not yet registered a name and return appropriate response to the client
 def register_user(request, address):
-	# Also prevents repeated registering
-	if isRegistered(request['handle'], address):
+
+	if not isConnected(address):
 		error = {
 			"command": 'error',
-			"message": 'Error: Registration failed, handle or alias already exists'
+			"message": "> Error: Registration failed. Port number is not connected to server."
+		}
+		return error
+
+	# Also prevents repeated registering
+	elif isRegistered(request['handle'], address):
+		error = {
+			"command": 'error',
+			"message": '> Error: Registration failed. Handle or alias already exists.'
 		}
 		
 		return error
@@ -34,10 +64,10 @@ def register_user(request, address):
 		registered_users.append({ "username": request['handle'], "address": address})
 		success = {
 			"command": 'success',
-			"message": 'Registration successful. Welcome ' + request['handle']
+			"message": '> Registration successful. Welcome, ' + request['handle'] + '!'
 		}
 
-		print('Current list of registered users: ', registered_users[0]['username'])
+		print('Current list of registered users: ', registered_users)
 		return success
 
 
@@ -45,7 +75,7 @@ def register_user(request, address):
 def receiveMessages():
 	while True:
 		try:
-			msg, adr = sock.recvfrom(1024)
+			msg, adr = server.recvfrom(1024)
 
 			dict_req = json.loads(msg)
 
@@ -62,15 +92,23 @@ def broadcastMessages():
 			q = messages.get()
 			message = q[0]
 			address = q[1]
+
+
+			if message['command'] == "join":
+				res = join_user(address)
+
+				json_response = json.dumps(res).encode('utf-8')
+
+				server.sendto(json_response, address)
 				
-			if message['command'] == "register":
+			elif message['command'] == "register":
 				#process registration, results are in the form of a dict
 				res = register_user(message, address)
 
 				#convert dictionary to JSON (for interpoability), encode into bytes
 				json_response = json.dumps(res).encode('utf-8')
 			
-				sock.sendto(json_response, address)
+				server.sendto(json_response, address)
 
 			elif message['command'] == "all":
 				pass
@@ -78,14 +116,21 @@ def broadcastMessages():
 			elif message['command'] == "msg":
 				pass
 
-			elif message['command'] == "join":
-				pass
-
 			elif message['command'] == "leave":
 				pass
 
 
-# Check if user is registered
+#Check if an address is already connected
+def isConnected(address):
+	ip, port = address
+	for adr in addresses:
+		if adr == address or udp_port == port:
+			return True
+	
+	return False
+
+
+# Check if user is already registered or username is taken
 def isRegistered(username, address):
 	for user in registered_users:
 		if user['username'] == username or user['address'] == address:
